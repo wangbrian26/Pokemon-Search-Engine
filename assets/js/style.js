5eb0becaec8616ef8e75ad4769dfb44f90ddfe78
Brian Wang <wangbrian26@gmail.com>
2022-11-18 14:44:29 -0800

    added logic for getPokemon function in js

---
+++ assets/js/style.js
@@ -0,0 +1,47 @@
+var url = "https://pokeapi.co/api/v2/pokemon";
+var pokeInfo = document.querySelector("#pokeInfo");
+var submitButton = document.querySelector("#submit");
+
+function getPokemon() {
+  var pokemonName = document.querySelector("#pokemonName");
+  pokemonName = pokemonName.value;
+  pokemonName = pokemonName.toLowerCase();
+  pokemonName = pokemonName.replace(" ", "-");
+  pokemonName = pokemonName.replace(".", "");
+  console.log(pokemonName);
+  fetch(url + "/" + pokemonName)
+    .then(function (response) {
+      if (response.status !== 200) {
+        pokeInfo.textContent = "Please enter a valid Pokemon name.";
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var chosenPokeName = document.createElement("h3");
+      chosenPokeName.textContent = pokemonName.toUpperCase();
+      pokeInfo.appendChild(chosenPokeName);
+      var height = document.createElement("p");
+      height.textContent = "Height: " + data.height / 10 + " m";
+      pokeInfo.appendChild(height);
+      var weight = document.createElement("p");
+      weight.textContent = "Weight: " + data.weight / 10 + " kg";
+      pokeInfo.appendChild(weight);
+      var idNum = document.createElement("p");
+      idNum.textContent = "Pokemon ID: " + data.id;
+      pokeInfo.appendChild(idNum);
+      for (var i = 0; i < data.types.length; i++) {
+        var type = document.createElement("p");
+        type.textContent = "Type: " + data.types[i].type.name;
+        pokeInfo.appendChild(type);
+      }
+      for (var i = 0; i < data.abilities.length; i++) {
+        var abilities = document.createElement("p");
+        abilities.textContent =
+          "This pokemon has the ability: " + data.abilities[i].ability.name;
+        pokeInfo.appendChild(abilities);
+      }
+    });
+}
+
+submitButton.addEventListener("click", getPokemon);
